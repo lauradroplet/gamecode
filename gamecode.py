17fed703877b2181b06c9f4b3578c2bb16346902
lauradroplet <gc20020101@gmail.com>
2020-08-04 22:19:46 -0700

    first edition

---
+++ gamecode.py
@@ -0,0 +1,12 @@
+while True:
+    mode = input('输入游戏模式')
+    if mode == '1':
+        print('launch mode 1')
+    elif mode == '2':
+        print('lanch mode 2')
+    elif mode == 'q':
+    	break
+    else:
+    	print('please input 1/2/q')
+
+
